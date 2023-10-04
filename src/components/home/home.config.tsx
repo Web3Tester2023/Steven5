8c73f1c2a614c3604d05158112c09cb618a55eb9
Web3Tester2023 <38856332+Web3Tester2023@users.noreply.github.com>
2023-10-04 20:59:13 +0800

    Update home.config.tsx

--- src/components/home/home.config.tsx
+++ src/components/home/home.config.tsx
@@ -1,40 +1,39 @@
 export const roadmapList = [
   {
     label: 'Current',
-    title: 'Q3 2023',
+    title: 'Phase #1: Unleash the APE's',
     list: [
-      'Launch of YANDER\'s official website, Socials and white paper',
-      'Tokenomics design and smart contract development',
-      'Community outreach and awareness building',
-      'Contract Audit',
-      'Presale Launch',
-      'DEX Listing'
+      'Token Launch & Presale',
+      'Listing on Major DEX & CEX Exchanges',
+      'Warrior Staking Platform',
+      'Community Building',
+      
     ]
   }, {
     label: '',
-    title: 'Q4 2023',
+    title: 'Phase #2: Scaling the Heights',
     list: [
-      'Begin building liquidity partnerships with centralized exchanges (CEX)',
-      'Launch the Youthful NFT collection, showcasing limited-edition artworks',
-      'Engage with the community to gather feedback and suggestions',
-      'Promote the YANDER brand through partnerships and sponsorships',
+      'Partnerships & Integrations',
+      'ApeSwap Ecosystem Integration',
+      'Ape-themed NFT Marketplace',
+      'Community Governance Enhancements',
     ]
   }, {
     label: '',
-    title: 'Q1 2024',
+    title: 'Phase #3: Dominating the Crypto Landscape',
     list: [
-      'Platform Development and Gamification',
-      'User Credits Based On $ AI Staking Balance',
-      'Public Wall For Memes Created By The Community'
+      'Token Utility Expansion',
+      'ApeWARRIOR Merchandise',
+      'ApeWARRIOR Mobile App'
     ]
   }, {
     label: '',
-    title: 'Q2 2024',
+    title: 'Phase #4: Reaching New Horizons',
     list: [
-      'Conduct security audits and ensure the platform\'s robustnes',
-      'Introduce YANDER merchandise collection and establish distribution channels',
-      'Continued marketing efforts to expand',
-      'Continue to spread joy and happiness to the blockchain!'
+      'Cross-Chain Integration',
+      'Strategic Acquisitions',
+      'Global Community Expansion',
+      'Charity & Philanthropy'
     ]
   }
 ]
